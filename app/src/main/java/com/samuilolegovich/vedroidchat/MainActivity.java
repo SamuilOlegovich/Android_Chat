10ffa4c9f9c293ff4701a126682618507e8acf27
SamuilOlegovich <47562089+SamuilOlegovich@users.noreply.github.com>
2020-11-25 16:48:42 +0200

    Add

--- app/src/main/java/com/samuilolegovich/vedroidchat/MainActivity.java
+++ app/src/main/java/com/samuilolegovich/vedroidchat/MainActivity.java
@@ -1,11 +1,13 @@
 package com.samuilolegovich.vedroidchat;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.annotation.Nullable;
 
 import android.text.format.DateFormat;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.ListView;
@@ -13,22 +15,41 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.firebase.ui.database.FirebaseListOptions;
+import com.github.library.bubbleview.BubbleTextView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.firebase.ui.database.FirebaseListAdapter;
 import com.google.firebase.auth.FirebaseAuth;
 import com.firebase.ui.auth.AuthUI;
 
+import hani.momanii.supernova_emoji_library.Actions.EmojIconActions;
+import hani.momanii.supernova_emoji_library.Helper.EmojiconEditText;
 
 
 
 public class MainActivity extends AppCompatActivity {
     private FirebaseListAdapter<Message> adapter;
     private RelativeLayout mainRelativeLayout;
+    private EmojiconEditText emojiconEditText;
     private FloatingActionButton sendButton;
+    private EmojIconActions emojIconActions;
+    private ImageView emojiButton;
+
+
     private static int SING_IN_CODE = 1;
 
+
+    // для подключения базы данных
+    // для авторизации
+    private FirebaseAuth firebaseAuth;
+    // для подключения к базе данных
+    private FirebaseDatabase firebaseDatabase;
+    // для работы с таблицами внутри базы данных
+    private DatabaseReference databaseReference;
+    private View main_relative_layout;
 
 
     @Override
@@ -52,19 +73,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference("Messages");
+
+
         mainRelativeLayout = (RelativeLayout) findViewById(R.id.main_relative_layout);
         sendButton = (FloatingActionButton)findViewById(R.id.button_send_message);
+        emojiconEditText = (EmojiconEditText) findViewById(R.id.input_text_field);
+        emojiButton = (ImageView) findViewById(R.id.emoji_button);
+        // позволяет вызывать клавиатуру с эмоджи
+        emojIconActions = new EmojIconActions(getApplicationContext(), MainActivity.this,
+                emojiconEditText, emojiButton) ;
+        emojIconActions.ShowEmojIcon();
+
+
+
         // привязываем к кнопке событие
         sendButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                EditText textField = findViewById(R.id.input_text_field);
-                if (textField.getText().toString().equals("")) return;
+                if (emojiconEditText.getText().toString().equals("")) return;
                 FirebaseDatabase.getInstance().getReference().push().setValue(
                         new Message(FirebaseAuth.getInstance().getCurrentUser().getEmail(),
-                                textField.getText().toString())
+                                emojiconEditText.getText().toString())
                 );
-                textField.setText("");
+                emojiconEditText.setText("");
             }
         });
 
@@ -78,7 +113,8 @@ public class MainActivity extends AppCompatActivity {
          *     implementation 'com.google.firebase:firebase-auth:16.0.1'
          *     implementation 'com.google.firebase:firebase-database:16.0.1'
          */
-        if (FirebaseAuth.getInstance().getCurrentUser() == null) {
+////        if (FirebaseAuth.getInstance().getCurrentUser() == null) {
+        if (firebaseAuth.getCurrentUser() == null) {
             /**
              * класс - AuthUI надо импортировать, для этого в build.gradle прописываем -->
              *     implementation 'com.firebaseui:firebase-ui:0.6.2'
@@ -88,6 +124,7 @@ public class MainActivity extends AppCompatActivity {
         } else {
             Snackbar.make(mainRelativeLayout, "Добро пожаловать :)", Snackbar.LENGTH_LONG).show();
         }
+
         displayAllMessage();
     }
 
@@ -98,21 +135,28 @@ public class MainActivity extends AppCompatActivity {
         ListView listOfMessage = findViewById(R.id.list_of_message);
         // добавляем данные(лист) в адаптор
         // классв котором выводим и класс чего выводим
-        adapter = new FirebaseListAdapter<Message>(this, Message.class,
-                // стили вывода и подключение к базе данных
-                R.layout.list_item, FirebaseDatabase.getInstance().getReference()) {
+        FirebaseListOptions<Message> options = new FirebaseListOptions.Builder<Message>()
+                .setQuery(FirebaseDatabase.getInstance().getReference(), Message.class)
+                .setLayout(R.layout.list_item)
+                .build();
+        adapter = new FirebaseListAdapter<Message>(options) {
+
+//        adapter = new FirebaseListAdapter<Message>(this, Message.class,
+//                // стили вывода и подключение к базе данных
+//                R.layout.list_item, FirebaseDatabase.getInstance().getReference()) {
             @Override
             protected void populateView(View v, Message model, int position) {
                 // list_item - тут у нас прописано три типа вывод, вот их мы и создаем и задаем данные
                 // v. - говорит что мы работак=ем внутри окна
                 TextView nameUser = v.findViewById(R.id.message_user);
                 TextView messageTime = v.findViewById(R.id.message_time);
-                TextView messageText = v.findViewById(R.id.message_text);
+                // тут мы наследуемся от класса BubbleTextView вмнсто TextView потому что
+                // применяем визуальное оформление к сообщению от BubbleTextView
+                BubbleTextView messageText = v.findViewById(R.id.message_text);
 
                 nameUser.setText(model.getUserName());
                 messageText.setText(model.getTextMessage());
                 messageTime.setText(DateFormat.format("dd-mm-yyyy HH:mm:ss", model.getMessageTime()));
-
             }
         };
         listOfMessage.setAdapter(adapter);
